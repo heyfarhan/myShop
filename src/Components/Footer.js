6263f1febd3378f76cfe6ba2fb0ab8198e04a5cf
heyfarhan03 <hey.farhan03@gmail.com>
2024-01-05 22:50:22 +0530

    removed

--- src/Components/Footer.js
+++
@@ -1,8 +0,0 @@
-const Footer = () => {
-    return (
-        <div className="h-14 w-full bg-gray-950 flex items-center justify-center py-2 font-medium text-white">
-            © Farhan Ahmed 2024 | Site Build For : Build with Innovation Private Limited
-        </div >
-    )
-}
-export default Footer
--- src/Components/ShimmerCard.js
+++
@@ -1,9 +0,0 @@
-const ShimmerCard = () => {
-    return (
-        <div className="h-84 w-64 m-2 p-2 flex flex-col gap-2 animate-pulse">
-            <div className='h-[25vh] bg-gray-300 rounded'></div>
-            <div className="  h-8 bg-gray-300 rounded"></div>
-        </div >
-    )
-}
-export default ShimmerCard
